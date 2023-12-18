6b19114ec175c1d0e499fef5c696f7400e7e708f
Abdüssamet Çelik <105125358+abdussametcelik@users.noreply.github.com>
2023-12-18 23:48:01 +0300

    Add files via upload

---
+++ app_api/models/db.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose")
+
+var dbURI = 'mongodb+srv://mekan:m1e2k3a4n@mekanbul.8wpvwdc.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresindeki veritabanına bağlanıldı.");
+});
+
+mongoose.connection.on("error",function(){
+    console.log("Bağlantı sağlanamadı.");
+});
+
+mongoose.connection.on("disconnected",function(){
+    console.log("Bağlantı kesildi.");
+});
+
+
+process.on("SIGINT", function(){
+    mongoose.connection.close();
+    console.log("Bağlantı Kapatıldı");
+    process.exit(0);
+});
+
+/*
+kapat=function(msg,callback){
+    mongoose.connection.close(function(){
+        console.log("Bağlantı Kapatıldı"+msg);
+        callback();
+    });
+    process.on("SIGINT",function(){
+        kapat("Uygulama Kapatıldı",function(){
+            process.exit(0);
+        });
+    });
+};
+*/
+
+require("./venue");
